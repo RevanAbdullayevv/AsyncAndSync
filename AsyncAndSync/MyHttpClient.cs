8184fee78b0881bc37e6a745b5c1f6407df151ad
Revan Abdullayev <rvana@LAPTOP-GOQERQ99>
2025-04-16 23:39:32 +0400

    Add project files.

---
+++ AsyncAndSync/MyHttpClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAndSync
+{
+    public class MyHttpClient
+    {
+        public static async Task GetData(List<string> urls)
+        {
+            MyHttpClient client = new MyHttpClient();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            foreach (var url in urls)
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"request send: {url}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"failed: {url} - Status: {response.StatusCode}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error with {url}: {ex.Message}");
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Total time: {stopwatch.Elapsed.TotalSeconds} seconds");
+        }
+
+        private async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ AsyncAndSync/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace AsyncAndSync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> urls = new List<string>()
+            {
+               "https://www.google.com",
+               "https://www.instagram.com",
+               "https://www.youtube.com",
+               "https://www.tiktok.com",
+               "https://www.discord.com"
+            };
+            MyHttpClient.GetData(urls).Wait();
+
+        }
+
+        
+    }
+}
